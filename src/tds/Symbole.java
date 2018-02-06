ee21221bfaf834f8cc10aaeb7cdd5cd360bb887c
Hue Nam <huenam.ly@gmail.com>
2018-02-06 17:28:07 +0100

    upadate symbole.java

--- src/tds/Symbole.java
+++ src/tds/Symbole.java
@@ -3,14 +3,19 @@ package tds;
 public class Symbole {
 	
 	protected int nombre_deplacement;
+	protected String type;
 	
-	
-	public Symbole(){
-		
+	public Symbole(int deplacement){
+		this.type = "entier";
+		this.nombre_deplacement = deplacement;
 	}
 	
 	public int getNombreDeplacement(){
 		return this.nombre_deplacement;
+	}
+	
+	public String getType(){
+		return this.type;
 	}
 	
 }
