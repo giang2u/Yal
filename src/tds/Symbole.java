4d0d33be058061161f0a9e6a2ad3028c348598d6
Hue Nam <huenam.ly@gmail.com>
2018-02-06 16:57:02 +0100

    add Symbole.java

--- src/tds/Symbole.java
+++ src/tds/Symbole.java
@@ -2,4 +2,15 @@ package tds;
 
 public class Symbole {
 	
+	protected int nombre_deplacement;
+	
+	
+	public Symbole(){
+		
+	}
+	
+	public int getNombreDeplacement(){
+		return this.nombre_deplacement;
+	}
+	
 }
